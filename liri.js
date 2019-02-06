39a83b302351eeaddcf39f16db424be06fd0b8c6
BBadoo <bbader0806@gmail.com>
2019-02-05 22:40:46 -0500

    readme

--- liri.js
+++ liri.js
@@ -5,21 +5,19 @@ var Omdb = require("omdb")
 var dotenv = require("dotenv");
 var moment = require("moment");
 var axios = require("axios");
+var Table = require("cli-table");
 var spotifyKey = (keys.spotify);
-var allBands;
 var userQuery = "";
 var operator = process.argv[2]
 var spotify = new Spotify(keys.spotify)
 var omdb = (keys.omdb)
 var fs = require("fs")
-// moment().format()
-// var now = moment().format("L");
 
 for (var i = 3; i < process.argv.length; i++) {
     if (i >= 4) {
-        userQuery += (" " + process.argv[i])
+        userQuery += (" " + process.argv[i].toLowerCase())
     } else {
-        userQuery = process.argv[i]
+        userQuery = (process.argv[i]).toLowerCase()
     }
 
 }
@@ -34,7 +32,11 @@ switch (operator) {
         break;
 
     case "movie-this":
-            OMDBF();
+        if (userQuery == "contact") {
+            console.log("This liri-app is currently unable to return search results on movies with a rating less than 1/10.")
+        } else {
+            OMDBF()
+        };
         break;
 
     case "do-what-it-says":
@@ -47,31 +49,43 @@ function OMDBF() {
     axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=" + omdb.id).then(
         function (response) {
             thisMovie = response.data
-            console.log(thisMovie.Title)
-            console.log(thisMovie.Year)
-            console.log(thisMovie.imdbRating)
-            console.log(thisMovie.Ratings[1].Source + " " + thisMovie.Ratings[1].Value)
-            console.log(thisMovie.Country)
-            console.log(thisMovie.Language)
-            console.log(thisMovie.Plot)
-            console.log(thisMovie.Actors)
+            var table = new Table({
+            })
+            table.push(
+
+                {"Title": thisMovie.Title},
+                {"Year Produced": thisMovie.Year},
+                {"imdb Rating": thisMovie.imdbRating},
+                {"Rotten Tomatoes Rating: ": thisMovie.Ratings[1].Value},
+                {"Country": thisMovie.Country},
+                {"Language": thisMovie.Language},
+                {"Plot": thisMovie.Plot},
+                {"Actors": thisMovie.Actors},
+            )
+            console.log(table.toString())
         }
     );
 }
 
 function spotified() {
     spotify.search({ type: 'track', query: userQuery }
-    , function (err, data) {
-        if (err) {
-            console.log("Error occurred: " + err);
-            return;
-        }
-        currentSpotify = data.tracks.items[0]
-        console.log("The Artist's name is " + currentSpotify.artists[0].name + ".")
-        console.log("The song name is " + currentSpotify.name + ".")
-        console.log("It is from the Album " + currentSpotify.album.name + ".")
-        console.log("Click here to preview the track " + currentSpotify.external_urls.spotify + " .")
-    })
+        , function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+            currentSpotify = data.tracks.items[0]
+            var table = new Table({
+                head: ["Artist Name", "Song", "Album", "Preview URL"],
+                colwidths: [100, 200]
+            })
+            table.push(
+                [currentSpotify.artists[0].name, currentSpotify.name,
+                currentSpotify.album.name, currentSpotify.external_urls.spotify,
+
+                ])
+            console.log(table.toString())
+        })
 }
 
 function concert() {
@@ -79,17 +93,23 @@ function concert() {
         function (response) {
             bandsArray = response.data.map(function (event) {
                 return { name: event.venue.name, place: event.venue.city + ", " + event.venue.country, date: event.datetime }
+
             })
-            console.log(bandsArray)
+            var table = new Table({
+                head: ["Venue", "Located in", "Date and Time"],
+                colwidths: [80, 80]
+            })
             for (k = 0; k < bandsArray.length; k++) {
-                console.log("Name of Venue: " + bandsArray[k].name + ", located in: " + bandsArray[k].place)
                 showDate = moment(bandsArray[k].date).format("L")
                 showTime = moment(bandsArray[k].date).format("LT")
+                table.push(
+                    [bandsArray[k].name, bandsArray[k].place,
+                    showDate + " at " + showTime,
 
-                console.log("Date and time of the event: " + showDate + " at " + showTime + " local time.")
-                console.log("********************")
+                    ])
 
             }
+            console.log(table.toString())
         }
     ).catch(function (err) {
         console.log("an error occurred")
@@ -97,27 +117,27 @@ function concert() {
 }
 
 function doit() {
-    fs.readFile("./random.txt", "utf-8", function(err, data) {
+    fs.readFile("./random.txt", "utf-8", function (err, data) {
         randomTextArray = data.split(",")
         userQuery = randomTextArray[1]
         switch (randomTextArray[0]) {
             case "concert-this":
                 concert();
                 break;
-        
+
             case "spotify-this-song":
                 spotified();
                 break;
-        
+
             case "movie-this":
-                    OMDBF();
+                OMDBF();
                 break;
-        
+
             case "do-what-it-says":
                 console.log("do-what-it-says");
                 doit()
                 break;
         }
     })
-   
+
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,39 @@
+#Liri-node-app
 
+What does it do?
+
+It can do whatever you want, as long as you want to search for a song, movie, find a band on tour, or pull simple commands from a text file.
+
+This project incorporates several NPM packages including:
+    1) node-spotify-api,
+    2) Omdb,
+    3) dotenv,
+    4) moment,
+    5) axios,
+    6) cli-tables, and
+    7) fs,
+
+##How to use it:
+
+###To find a song on spotify:
+    On the command line:
+        node liri.js spotify-this-song 
+            followed by your song of choice.
+            This will create a table with the Artist's Name, Song Name, Album Name, and a preview URL link.
+
+###To find a movie on omdb:
+    On the command line:
+        node liri.js movie-this
+            followed by your movie of choice.
+            this will create a table with the movie title, year the movie was made, the IMDB rating, the Country the movie was produced in, the Lnaguages the movie can be heard in, the basic plot of the movie, and the actors in the movie.
+
+###To find a band on tour:
+    On the command line:
+        node liri.js concert-this
+            followed by your band of choice.
+            This will create a table with the venue name, city and country, as well as the date and time of the show.
+
+###To do what the text file says:
+    On the command line:
+        node liri.js do-what-it-says
+            This will load values from the text file random.txt and run the corresponding function with preloaded values.
